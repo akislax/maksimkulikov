28bb48ee1011de9ba4861a35d8f08a6b543077b5
makimaks <maximkulikovmkv@gmail.com>
2026-05-07 19:41:58 +0700

    laba2

--- laba2/kotlin/1.8.kt
+++ laba2/kotlin/1.8.kt
@@ -1,37 +1,45 @@
 fun main() {
     println("Введите строку S: ")
-    var s = readln().trim() 
+    var s = readln().trim()
     println("Введите строку T: ")
-    var t = readln().trim() 
+    var t = readln().trim()
     var result = ""
-    for (i in 0 until s.length){
-        for( j in 1..s.length - i){
+    
+    for (i in 0 until s.length) {
+        for (j in t.length..s.length - i) {
             var part = s.substring(i, i + j)
             var part_1 = part
             var ch = 0
-            if (part.length == t.length) {
-                for (k in 0 until t.length){
-                    var count_t = 0
-                    var count_part = 0
-                    for (m in 0 until t.length){
-                        if (t[m] == part[k]){
-                            count_t++
-                        }
-                        if (part[m] == part[k]){
-                            count_part++
-                        }
-                    }
-                    if (count_part > count_t){
-                        ch++
+
+            for (k in 0 until t.length) {
+                var count_t = 0
+                var count_part = 0
+                
+                for (m in 0 until t.length) {
+                    if (t[m] == t[k]) {
+                        count_t++
                     }
                 }
-                if (ch == 0) {
+                for (m in 0 until part.length) {
+                    if (part[m] == t[k]) {
+                        count_part++
+                    }
+                }
+                
+                if (count_part < count_t) {
+                    ch++
+                }
+            }
+        
+            if (ch == 0) {
+                if (result == "" || part.length < result.length) {
                     result = part
                 }
             }
         }
-    }
-    if (result == ""){
+    }   
+    
+    if (result == "") {
         println("Не найдено")
     } else {
         println("Результат: $result")
